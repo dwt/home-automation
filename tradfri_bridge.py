50d955579ca3c39ffbcda6073c55bcebe47aedc0
Martin Häcker <mhaecker@mac.com>
2025-02-04 21:39:33 +0100

    Remove unused imports and tone down the logging a bit to protect my raspi memory card

--- tradfri_bridge.py
+++ tradfri_bridge.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 
-import os
 import logging
 
 from pyhap.accessory import Accessory, Bridge
 from pyhap import const
-import pyhap.loader as loader
-import pyhap.util as util
 import subprocess
 
 logger = logging.getLogger(__name__)
@@ -14,16 +11,16 @@ logger = logging.getLogger(__name__)
 
 class Fnordlicht(Accessory):
     """A switch accessory that starts / stops the fnordlicht."""
-    
+
     category = const.CATEGORY_LIGHTBULB
-    
+
     def __init__(self, *args, **kwargs):
         """Initialise and set a shutdown callback to the On characteristic."""
         super().__init__(*args, **kwargs)
         serv_light = self.add_preload_service('Lightbulb')
         self.char_on = serv_light.configure_char(
             'On', setter_callback=self.toggle_fnordlicht)
-        
+
         self.subprocess = None
 
     def toggle_fnordlicht(self, should_turn_on):
@@ -35,13 +32,13 @@ class Fnordlicht(Accessory):
     def execute_turn_on(self):
         logger.info("Executing turn on command.")
         self.subprocess = subprocess.Popen(['./fnordlicht.py'])
-    
+
     def execute_turn_off(self):
         logger.info("Executing turn off command.")
         if self.subprocess is not None:
             self.subprocess.kill()
             self.subprocess = None
-    
+
     def stop(self):
         super().stop()
         logger.info('stopping')
@@ -49,15 +46,15 @@ class Fnordlicht(Accessory):
 
 class PhillipsHueLightStrip(Accessory):
     "A switch to control the Lightstrip"
-    
+
     category = const.CATEGORY_LIGHTBULB
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         light_service = self.add_preload_service(
             'Lightbulb', chars=['On', 'Hue', 'Saturation', 'Brightness']
         )
-        
+
         self.char_on = light_service.configure_char(
             'On', setter_callback=self.toggle_on
         )
@@ -70,15 +67,15 @@ class PhillipsHueLightStrip(Accessory):
         self.char_brightness = light_service.configure_char(
             'Brightness', setter_callback=self.set_brightness
         )
-        
+
         self.is_on = False
         self.hue = self.saturation = self.brightness = 0
         self.sync()
-    
+
     def toggle_on(self, should_be_on):
         self.is_on = should_be_on
         self.sync()
-    
+
     def set_hue(self, value):
         self.hue = value
         self.sync()
@@ -90,32 +87,32 @@ class PhillipsHueLightStrip(Accessory):
     def set_brightness(self, value):
         self.brightness = value
         self.sync()
-    
+
     def sync(self):
         hue, saturation, brightness = self.hue, self.saturation, self.brightness
         if not self.is_on:
             brightness = 0
-        
+
         subprocess.run([
-            './light_strip.py', 
-            '--hue', str(hue), 
+            './light_strip.py',
+            '--hue', str(hue),
             '--saturation', str(saturation),
             '--brightness', str(brightness),
             '--transition-seconds', str(1)
         ])
 
-        
+
 
 import signal
 from pyhap.accessory_driver import AccessoryDriver
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    
+    logging.basicConfig(level=logging.INFO)
+
     # The AccessoryDriver preserves the state of the accessory
     # (by default, in the below file), so that you can restart it without pairing again.
     driver = AccessoryDriver(port=51826, persist_file='fnordlicht.state')
-    
+
     bridge = Bridge(driver, 'Bridge')
     bridge.add_accessory(Fnordlicht(driver, 'Fnordlicht'))
     bridge.add_accessory(PhillipsHueLightStrip(driver, 'Lange Wand'))
